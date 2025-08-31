d33339f23f3e094b7e0c89232e98b2974b10981e
Zach Deibert <zachdeibert@gmail.com>
2025-08-31 13:35:07 -0500

    Create plugin

---
+++ Com/ZachDeibert/JellyfinPluginHDHomeRunGuide/Configuration/PluginConfiguration.cs
@@ -0,0 +1,7 @@
+namespace Com.ZachDeibert.JellyfinPluginHDHomeRunGuide.Configuration;
+
+using MediaBrowser.Model.Plugins;
+
+public class PluginConfiguration : BasePluginConfiguration {
+
+}
---
+++ Com/ZachDeibert/JellyfinPluginHDHomeRunGuide/Plugin.cs
@@ -0,0 +1,11 @@
+namespace Com.ZachDeibert.JellyfinPluginHDHomeRunGuide;
+
+using Com.ZachDeibert.JellyfinPluginHDHomeRunGuide.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+
+public class Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : BasePlugin<PluginConfiguration>(applicationPaths, xmlSerializer) {
+    public override Guid Id => Guid.Parse("0B4BBA13-E571-435C-8319-84D55E93A5DA");
+    public override string Name => "HDHomeRun Guide";
+}
