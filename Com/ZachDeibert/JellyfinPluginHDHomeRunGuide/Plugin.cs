37f84818e9e9eb49f6d049f7b08a153b2b579bb6
Zach Deibert <zachdeibert@gmail.com>
2025-09-01 12:16:57 -0500

    Add description

--- Com/ZachDeibert/JellyfinPluginHDHomeRunGuide/Plugin.cs
+++ Com/ZachDeibert/JellyfinPluginHDHomeRunGuide/Plugin.cs
@@ -6,6 +6,7 @@ using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Serialization;
 
 public class Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : BasePlugin<PluginConfiguration>(applicationPaths, xmlSerializer) {
+    public override string Description => "Pull guide data from HDHomeRun";
     public override Guid Id => Guid.Parse("0B4BBA13-E571-435C-8319-84D55E93A5DA");
     public override string Name => "HDHomeRun Guide";
 }
